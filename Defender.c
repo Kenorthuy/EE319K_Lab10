56d31efe870679c5da5900eda63826483145e90e
Kenorthuy <55063699+Kenorthuy@users.noreply.github.com>
2020-04-24 21:57:09 -0500

    added separate sounds and images folders, worked mainly on getting sounds to work

---
+++ Defender.c
@@ -0,0 +1,151 @@
+// Defender.c
+// Runs on LM4F120/TM4C123
+// Evan Varghese and Ken Nguyen
+
+// Last Modified: 1/17/2020 
+
+// GAMEPLAY OVERVIEW
+// http://www.digitpress.com/dpsoundz/mp3/conquer_the_video_craze/conquer_the_video_craze_03_-_Defender.mp3
+
+// sounds at https://seanriddle.com/willy2.html
+// 				and http://www.digitpress.com/dpsoundz/soundfx.htm
+
+
+/* This example accompanies the books
+   "Embedded Systems: Real Time Interfacing to Arm Cortex M Microcontrollers",
+   ISBN: 978-1463590154, Jonathan Valvano, copyright (c) 2019
+
+   "Embedded Systems: Introduction to Arm Cortex M Microcontrollers",
+   ISBN: 978-1469998749, Jonathan Valvano, copyright (c) 2019
+
+ Copyright 2019 by Jonathan W. Valvano, valvano@mail.utexas.edu
+    You may use, edit, run or distribute this file
+    as long as the above copyright notice remains
+ THIS SOFTWARE IS PROVIDED "AS IS".  NO WARRANTIES, WHETHER EXPRESS, IMPLIED
+ OR STATUTORY, INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE.
+ VALVANO SHALL NOT, IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL,
+ OR CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
+ For more information about my classes, my research, and my books, see
+ http://users.ece.utexas.edu/~valvano/
+ */
+ 
+// ******* Hardware I/O connections*******************
+// Slide pot pin 1 connected to ground
+// Slide pot pin 2 connected to PD2/AIN5
+// Slide pot pin 3 connected to +3.3V 
+// Left button connected to PE0
+// Right button connected to PE1
+// Fire Button connected to PE2
+// 32*R resistor DAC bit 0 on PB0 (least significant bit)
+// 16*R resistor DAC bit 1 on PB1
+// 8*R resistor DAC bit 2 on PB2
+// 4*R resistor DAC bit 3 on PB3
+// 2*R resistor DAC bit 4 on PB4
+// 1*R resistor DAC bit 5 on PB5 (most significant bit)
+
+// LED on PB4
+// LED on PB5
+
+// Backlight (pin 10) connected to +3.3 V
+// MISO (pin 9) unconnected
+// SCK (pin 8) connected to PA2 (SSI0Clk)
+// MOSI (pin 7) connected to PA5 (SSI0Tx)
+// TFT_CS (pin 6) connected to PA3 (SSI0Fss)
+// CARD_CS (pin 5) unconnected
+// Data/Command (pin 4) connected to PA6 (GPIO), high for data, low for command
+// RESET (pin 3) connected to PA7 (GPIO)
+// VCC (pin 2) connected to +3.3 V
+// Gnd (pin 1) connected to ground
+
+#include <stdint.h>
+#include "../inc/tm4c123gh6pm.h"
+#include "ST7735.h"
+#include "Print.h"
+#include "Random.h"
+#include "PLL.h"
+#include "ADC.h"
+#include "Images.h"
+#include "Sound.h"
+#include "Timer0.h"
+#include "Timer1.h"
+
+void DisableInterrupts(void); // Disable interrupts
+void EnableInterrupts(void);  // Enable interrupts
+void Delay100ms(uint32_t count); // time delay in 0.1 seconds
+
+typedef enum {dead,alive} status_t;
+struct sprite {
+  int32_t x;      // x coordinate
+  int32_t y;      // y coordinate
+  int32_t vx,vy;  // pixels/30Hz
+  const unsigned short *image; // ptr->image
+  const unsigned short *black;
+  status_t life;        // dead/alive
+  int32_t w; // width
+  int32_t h; // height
+  uint32_t needDraw; // true if need to draw
+};
+typedef struct sprite sprite_t;
+
+
+struct laser {
+	int32_t x;
+	int32_t y;
+	int32_t vx,vy;
+	const unsigned short *image;
+	int32_t w;
+	int32_t h;
+};
+typedef struct laser laser_t;
+
+
+int main(void){
+  DisableInterrupts();
+  PLL_Init(Bus80MHz);       // Bus clock is 80 MHz 
+  Random_Init(1);
+
+  Output_Init();
+  ST7735_FillScreen(0x0000);            // set screen to black
+  
+  ST7735_DrawBitmap(22, 159, PlayerShip0, 18,8); // player ship bottom
+  ST7735_DrawBitmap(53, 151, Bunker0, 18,5);
+  ST7735_DrawBitmap(42, 159, PlayerShip1, 18,8); // player ship bottom
+  ST7735_DrawBitmap(62, 159, PlayerShip2, 18,8); // player ship bottom
+  ST7735_DrawBitmap(82, 159, PlayerShip3, 18,8); // player ship bottom
+
+  ST7735_DrawBitmap(0, 9, SmallEnemy10pointA, 16,10);
+  ST7735_DrawBitmap(20,9, SmallEnemy10pointB, 16,10);
+  ST7735_DrawBitmap(40, 9, SmallEnemy20pointA, 16,10);
+  ST7735_DrawBitmap(60, 9, SmallEnemy20pointB, 16,10);
+  ST7735_DrawBitmap(80, 9, SmallEnemy30pointA, 16,10);
+  ST7735_DrawBitmap(100, 9, SmallEnemy30pointB, 16,10);
+
+  Delay100ms(50);              // delay 5 sec at 80 MHz
+
+  ST7735_FillScreen(0x0000);   // set screen to black
+  ST7735_SetCursor(1, 1);
+  ST7735_OutString("GAME OVER");
+  ST7735_SetCursor(1, 2);
+  ST7735_OutString("Nice try,");
+  ST7735_SetCursor(1, 3);
+  ST7735_OutString("Earthling!");
+  ST7735_SetCursor(2, 4);
+  LCD_OutDec(1234);
+  while(1){
+  }
+
+}
+
+
+// You can't use this timer, it is here for starter code only 
+// you must use interrupts to perform delays
+void Delay100ms(uint32_t count){uint32_t volatile time;
+  while(count>0){
+    time = 727240;  // 0.1sec at 80 MHz
+    while(time){
+	  	time--;
+    }
+    count--;
+  }
+}
