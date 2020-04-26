79213a59d52225646e0f494baaaea7476fcb25a1
Evan <varghese.evan@gmail.com>
2020-04-26 13:43:09 -0500

    update creatures

--- Creatures.h
+++ Creatures.h
@@ -11,8 +11,10 @@ typedef struct {
 	uint8_t ypos;     // how long the array is
 	uint8_t width;     // how long the array is
 	uint8_t height;     // how long the array is
+	uint8_t pickedup;		//property unique to astronauts for tracking vertical position
+	uint8_t carrying;		//property unique to landers for moving humans out
 } creature_t;
 
-extern creature_t enemies[1];
+extern creature_t enemies[2];
 
 extern creature_t humans[1];
